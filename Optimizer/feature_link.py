b10ebe908ef4bde5c2a3df8fe7aff6b4bc5bbe94
Patrick Xia <txia4@ncsu.edu>
2018-02-09 23:22:09 -0500

    update transform

--- Optimizer/feature_link.py
+++ Optimizer/feature_link.py
@@ -5,14 +5,13 @@ import pandas as pd
 import numpy as np
 from ABE.main import abe_execute
 from ABE.main import gen_setting_obj
-from FeatureModel.Feature_tree import FeatureTree
-# from Optimizer.de_test import ind1
 from utils.kfold import KFoldSplit
 
 
 def transform(x=None):
     if x is None:
-        x = [0, 0, 0, 0, 0, 0]
+        x = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    x = x.tolist()
     fm2S = [
         ['rm_noting', 'outlier', 'prototype'],
         ['remain_same', 'genetic_weighting', 'gain_rank', 'relief', 'principal_component', 'cfs', 'consistency_subset',
@@ -22,15 +21,13 @@ def transform(x=None):
         ['median_adaptation', 'mean_adaptation', 'second_learner_adaption', 'weighted_mean'],
         ['analogy_fix1', 'analogy_fix2', 'analogy_fix3', 'analogy_fix4', 'analogy_fix5', 'analogy_dynamic']
     ]
-
+    x = map(int, x)
     setting_str = list()
 
     for i, v in enumerate(x):
         setting_str.append(fm2S[i][v])
 
-    # print(setting_str)
     settings_1 = gen_setting_obj(setting_str)
-    # print(settings_1)
 
     logging.basicConfig(stream=sys.stdout,
                         format='[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s',
@@ -41,16 +38,12 @@ def transform(x=None):
         trainData = pd.DataFrame(data=train)
         testData = pd.DataFrame(data=test)
         error = abe_execute(S=settings_1, train=trainData, test=testData)
-        # print(error)
         ERR.append(error)
 
-    # print(np.mean(ERR))
-    return np.mean(ERR)
+    return np.mean(ERR),
 
 
-def convert(x=None):
-    if x is None:
-        x = [0, 0, 0, 0, 0, 0]
+def convert(x):
     fm3S = [
         ['rm_noting', 'outlier', 'prototype'],
         ['remain_same', 'genetic_weighting', 'gain_rank', 'relief', 'principal_component', 'cfs', 'consistency_subset',
@@ -61,10 +54,10 @@ def convert(x=None):
         ['analogy_fix1', 'analogy_fix2', 'analogy_fix3', 'analogy_fix4', 'analogy_fix5', 'analogy_dynamic']
     ]
 
+    x = map(int, x)
     setting_str = list()
     for i, v in enumerate(x):
         setting_str.append(fm3S[i][v])
 
     return setting_str
 
-    # print(convert())
