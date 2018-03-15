7a33226160b931daddaee00becaa1ac328db3a9b
Patrick Xia <txia4@ncsu.edu>
2018-03-15 10:43:00 -0400

    Add a local run method

--- results/experiment.py
+++ results/experiment.py
@@ -22,6 +22,7 @@
 #  THE SOFTWARE.
 
 import sys
+import warnings
 
 from data.new_data import data_albrecht, data_desharnais, data_finnish, data_kemerer, data_maxwell, data_miyazaki
 from results.methods import de_estimate, random_strategy, abe0_strategy
@@ -73,10 +74,49 @@ def hpc():
             elif methodologyId == 3:
                 res = ABE0(train, test)
 
-            with open('FINAL.txt', 'a+') as f:
+            with open('test.txt', 'a+') as f:
                 f.write(
                     '^^^ ' + sys.argv[1] + ' ' + sys.argv[2] + ' ' + str(res[0]) + ' ' + str(res[1]) + '\n')
 
 
+def local_run():
+    """
+    - system auguments:
+        1 modelIndex  [0-5]
+        2 methodology ID [0-DE2, 1-DE8, 2-RANDOM, 3-ABE0]
+    :return: writing to sysout
+        ^^^^ repeatID mre sa
+    """
+    data_id = 1    ################# dataset used  [0-5]
+    method_id = 3  ################# method used  [0-DE2, 1-DE8, 2-RANDOM, 3-ABE0]
+    # print("data_id: " + str(data_id) + " " + "method_id: " + str(method_id))
+
+    datafunc = [data_albrecht, data_desharnais, data_finnish, data_kemerer, data_maxwell, data_miyazaki]
+    model = datafunc[data_id]
+    num_pj = len(model())
+    res = None
+
+    if num_pj < 40:
+        fold_num = 3
+    else:
+        fold_num = 10
+
+    for train, test in KFoldSplit_df(model(), fold_num):
+        if method_id == 0:
+            res = DE2(train, test)
+        elif method_id == 1:
+            res = DE8(train, test)
+        elif method_id == 2:
+            res = RANDOM(train, test)
+        elif method_id == 3:
+            res = ABE0(train, test)
+
+        with open('new_test1.txt', 'a+') as f:
+            f.write(
+                '### ' + str(data_id) + ' ' + str(method_id) + ' ' + str(res[0]) + ' ' + str(res[1]) + '\n')
+
+
 if __name__ == '__main__':
-    hpc()
+    repeats = 20     ################# repeat times
+    for _ in range(repeats):
+        local_run()
