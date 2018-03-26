502138fc61b1025534d6cce1e4d96ab05d919ca4
Patrick Xia <txia4@ncsu.edu>
2018-03-25 22:57:15 -0400

    updates

--- plots/sorting_result.py
+++ plots/sorting_result.py
@@ -4,19 +4,21 @@ import matplotlib.pyplot as plt
 from random import *
 
 # [0-albrecht, 1-desharnais, 2-finnish, 3-kemerer, 4-maxwell, 5-miyazaki, 6-china, 7-isbsg10, 8-kitchenham]
+data_index = 6
+
+data_list = ["albrecht", "desharnais", "finnish", "kemerer", "maxwell",
+             "miyazaki", "china", "isbsg10", "kitchenham"]
+
 # data = pd.read_csv('./final_list_cr0.3_f0.8.txt', sep=";", header=None)
 data = pd.read_csv('Outputs/final_list_cr0.3_f0.8.txt', sep=";", header=None)
 data.columns = ["Data_ID", "Method_ID", "MRE", "SA", "CONFIG"]
 
-whigham = pd.read_csv('./ATLM.txt', sep=";", header=None)
+whigham = pd.read_csv('Outputs/ATLM.txt', sep=";", header=None)
 whigham.columns = ["Data", "Method", "MRE", "SA"]
-# print(whigham)
 
-df15 = whigham.query('Data == ["kitchenham"] and Method == ["ATLM"]')
+df15 = whigham.query('Data == ["'+str(data_list[data_index])+'"] and Method == ["ATLM"]')
 df15_MRE = sorted(df15.loc[:,"MRE"])
 df15_SA = sorted(df15.loc[:,"SA"])
-
-data_index = 8
 
 df10 = data.query('Data_ID == ["'+str(data_index)+'"] and Method_ID == ["0"]')
 df10_MRE = sorted(df10.loc[:,"MRE"])
@@ -43,13 +45,7 @@ df14_MRE = sorted(df14.loc[:,"MRE"])
 df14_SA = sorted(df14.loc[:,"SA"])
 
 
-
-
-
-
-
 plt.figure(1)
-
 plt.plot(df10_MRE)
 plt.plot(df11_MRE)
 plt.plot(df12_MRE)
@@ -67,7 +63,6 @@ plt.legend(['ABE0', 'RD40', 'RD160', 'DE2', 'DE8', 'ATLM'], loc='upper left', fo
 plt.show()
 
 plt.figure(2)
-
 plt.plot(df10_SA)
 plt.plot(df11_SA)
 plt.plot(df12_SA)
